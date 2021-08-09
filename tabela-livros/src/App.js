51c7e114711428f9bd9a46daa33fa3a5ea49882d
DyogoViana <dyogo.vianaa@gmail.com>
2021-08-09 15:35:58 -0300

    [ Capítulo 002 ] : Criação do State > Livros || tabela body.

--- tabela-livros/src/App.js
+++ tabela-livros/src/App.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Tabelas
 import TabelaHead from "./components/tabelas/TabelaHead";
 import TabelaBody from "./components/tabelas/TabelaBody";
 import TabelaFoot from "./components/tabelas/TabelaFoot";
 
-function App() {
-	return (
-		<table className="tabela">
-			<TabelaHead />
-			<TabelaBody />
-			<TabelaFoot />
-		</table>
-	);
+class App extends Component {
+	state = {
+		livros: [
+			{
+				id: "978-85-7522-632-2",
+				titulo: "CSS Grid Layout",
+				autor: "Maurício Samy Silva"
+			},
+			{
+				id: "978-85-7522-677-3",
+				titulo: "Node Essencial",
+				autor: "Ricardo R. Lecheta"
+			},
+			{
+				id: "978-85-7522-512-7",
+				titulo: "Aprendendo Material Design",
+				autor: "Kyle Mew"
+			}
+		]
+	}
+	render() {
+		return (
+			<table className="tabela">
+				<TabelaHead />
+				<TabelaBody livros={ this.state.livros }/>
+				<TabelaFoot />
+			</table>
+		);
+	}
 }
-
 export default App;
