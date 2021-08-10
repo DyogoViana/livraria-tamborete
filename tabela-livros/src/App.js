0860d33690e3d04b69037bbfb662757ebb20fdcf
DyogoViana <dyogo.vianaa@gmail.com>
2021-08-10 15:01:03 -0300

    [ Capítulo 02 ] : Ordenando a lista de livros na ordem crescente e decrescente.

--- tabela-livros/src/App.js
+++ tabela-livros/src/App.js
@@ -12,26 +12,46 @@ class App extends Component {
 
 	componentDidMount() {
 		fetch("/api/livros.json")
-		 .then(response => response.json())
-		 .then(livros => this.setState({ livros }))
-		 .catch(function(error) {
-			 console.log("Erro na requisição.");
-		 })
-		 .finally(function() {
-			 console.log("Sempre retorna.");
-		 });
+			.then(response => response.json())
+			.then(livros => this.setState({ livros }))
+			.catch(function (error) {
+				console.log("Erro na requisição.");
+			})
+			.finally(function () {
+				console.log("Sempre retorna.");
+			});
 	};
 
 	// remover linha da tabela.
 	handleRemoverlinha = (id) => {
 		const livros = this.state.livros.filter(listaLivros => listaLivros.id !== id);
-		this.setState({livros});
+		this.setState({ livros });
 	}
+
+	// Classifica em ordem alfabética crescente e decrescente.
+	handleOrdenarCrescente = (titulo) => {
+		const livros = this.state.livros.sort((a, b) =>
+			a.titulo < b.titulo ? -1 : 0
+		);
+		this.setState({ livros });
+	};
+
+	handleOrdenarDecrescente = (titulo) => {
+		const livros = this.state.livros.sort((a, b) =>
+			a.titulo < b.titulo ? -1 : 0
+		);
+		livros.reverse();
+		this.setState({ livros });
+	}
+
 
 	render() {
 		return (
 			<table className="tabela">
-				<TabelaHead />
+				<TabelaHead
+					ordenarCrescente={this.handleOrdenarCrescente}
+					ordenarDecrescente={this.handleOrdenarDecrescente}
+				/>
 				<TabelaBody
 					livros={this.state.livros}
 					removerLinha={this.handleRemoverlinha}
