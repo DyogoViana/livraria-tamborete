1862f0d36787ad35b3a997adf6166e944370e172
DyogoViana <dyogo.vianaa@gmail.com>
2021-09-09 18:01:44 -0300

    [ Conteúdo dinâmico ] : Criação das páginas relacionadas aos links, buscando os livros pela API.

--- editora-tamborete/src/components/FrontEnd.js
+++ editora-tamborete/src/components/FrontEnd.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const FrontEnd = () => (
+const FrontEnd = ({ livros }) => (
 	<main className="principal">
 		<h2>Categoria Front End</h2>
-		<div className="card">
-			<p>Conteúdos da página Front End.</p>
-		</div>
+		{
+			livros
+				.filter(lista => lista.categoria === "frontend")
+				.map(livro => (
+					<div className="card" key={livro.id}>
+						<div className="thumb">
+							<img
+								src={"/imagens/capas/" + livro.id + ".jpg"}
+								alt="Thumbnails da capa do livro"
+							/>
+						</div>
+						{
+							livros
+								.filter(c => c.slug === livro.slug)
+								.map(livro => (
+									<span key={livro.slug}>
+										<Link to={`/livro/${livro.slug}`}>
+											{
+												<div className="detalhes">
+													<h3>{livro.titulo}</h3>
+													<p>{livro.descricao.slice(0, 130) + "..."}</p>
+													<p>Leia mais &gt;</p>
+												</div>
+											}
+										</Link>
+									</span>
+								))
+						}
+					</div>
+				))
+		}
 	</main>
 );
 
